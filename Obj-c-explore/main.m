2792b45021f35b04353efd4f7746dbf4f55cbd69
windwail <windwail>
2021-06-29 08:50:32 +0300

    Initial Commit

---
+++ Obj-c-explore/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Obj-c-explore
+//
+//  Created by Sergio on 29.06.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+  return 0;
+}
