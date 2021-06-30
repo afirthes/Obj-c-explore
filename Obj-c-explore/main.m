36dd0d329908073d12bbde2f9a9c68be885219aa
windwail <windwail>
2021-06-30 10:50:18 +0300

    comparing cached and non-cached calls

--- Obj-c-explore/main.m
+++ Obj-c-explore/main.m
@@ -12,7 +12,16 @@ int main(int argc, const char * argv[]) {
   @autoreleasepool {
     NSLog(@"\nHello, World!\n");
     RSPerson *object = [[RSPerson alloc] initWithName: @"Mike"];
+    
+    clock_t start = clock();
     [object sayHello];
+    clock_t end = clock();
+    NSLog(@"First call (without cache): %lu", end - start);
+    
+    start = clock();
+    [object sayHello];
+    end = clock();
+    NSLog(@"Second call (with cache): %lu", end - start);
   }
   return 0;
 }
